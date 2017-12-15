29834fd641dbade0a954ea75a7076aa5214cb586
Chris <463720769@qq.com>
2017-12-15 23:20:13 +0800

    图片处理代码格式化
    新加0.java 在上面做修改

--- code/xu-image-java/GSPic/src/ShadowShape0.java
+++ code/xu-image-java/GSPic/src/ShadowShape0.java
@@ -13,7 +13,7 @@ import javax.imageio.stream.ImageInputStream;
 import java.util.Iterator;
 import java.util.Random;
 
-public class ShadowShape {
+public class ShadowShape0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,7 +22,7 @@ public class ShadowShape {
 		String b="F:/git4elearning/Slide-CPATCHA/code/xu-image-java/slidePic/SHADOW/pic-000010011.";
 		String c="F:/git4elearning/Slide-CPATCHA/code/xu-image-java/slidePic/pic-00001000-11.";
 		try {
-			new ShadowShape().MShadowShape(a,b,c);
+			new ShadowShape0().MShadowShape(a,b,c);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,53 +34,45 @@ public class ShadowShape {
 	private Shape falseShape;
 	private int rule;
 	private float alpha;
-	private double sh_x;   //阴影区x坐标
-	private double sh_y;   //阴影区y坐标
-	private double ratio;  //比例参数，对不同图片，尤其是高清图片可以调整阴影区的大小
-	//private String baseFilePathIn="D:\\javaprojects\\pictures\\8761.jpg"; //图片读入路径
-	//private String baseFilePathOutShadow="D:\\javaprojects\\pictures\\shadow/b."; //带阴影图片输出路径及名称
-	//private String baseFilePathOutPiece="D:\\javaprojects\\pictures\\pieces/c."; //碎片图片输出路径及名称
-	private String getpiecePathPic;  //example:getpiecePathPic="D:\\javaprojects\\pictures\\pieces\\c.";
-	private double pic_x; //图片长宽设置
-	private double pic_y; //图片长宽设置
-	private int spin;   //旋转标识，0或1
-	final String picType = "jpeg";  //输入输出图片格式
-	final String picPieceType = "png";//图片碎片的格式只能是png
+	private double sh_x;
+	private double sh_y;
+	private double ratio;
+	private String getpiecePathPic;
+	private double pic_x;
+	private double pic_y;
+	private int spin;
+	final String picType = "jpeg";
+	final String picPieceType = "png";
 
 	public void MShadowShape(String baseFilePathIn,String baseFilePathOutShadow,String baseFilePathOutPiece) throws IOException{
 
 		File baseFile = new File(baseFilePathIn);
 		File shadowFile = new File(baseFilePathOutShadow + picType);
-	    File pieceFile = new File(baseFilePathOutPiece + picPieceType);
-	    String tmp=baseFilePathOutPiece.replace("-1", "-2");
-	    File pieceFileSwitch = new File(tmp + picPieceType);
-	    getpiecePathPic=baseFilePathOutPiece.replace("/", "\\");
-	    File getPieceFile = new File(getpiecePathPic + picPieceType);
-	    File getPieceFileSwitch = new File(tmp.replace("/", "\\") + picPieceType);
-	    ratio = 1;  //比例参数，更改调整阴影区大小
+	  File pieceFile = new File(baseFilePathOutPiece + picPieceType);
+	  String tmp=baseFilePathOutPiece.replace("-1", "-2");
+	  File pieceFileSwitch = new File(tmp + picPieceType);
+	  getpiecePathPic=baseFilePathOutPiece.replace("/", "\\");
+	  File getPieceFile = new File(getpiecePathPic + picPieceType);
+	  File getPieceFileSwitch = new File(tmp.replace("/", "\\") + picPieceType);
+	  ratio = 1;
 
-	    //图片读入
-	    BufferedImage SImg = null;
+	  BufferedImage SImg = null;
 		SImg = ImageIO.read(baseFile);
 		pic_x=SImg.getWidth();
 		pic_y=SImg.getHeight();
 
-		//画出阴影区碎片形状，并生成随机坐标x,y以及随机旋转标识spin=0或1，最后结果为得到shape
 		Random r=new Random();
-		sh_x=r.nextDouble()*(pic_x-200*ratio)+50*ratio;//scale is 50——pic_x-150
-		sh_y=r.nextDouble()*(pic_y-200*ratio)+50*ratio;//scale is 50——pic_y-150
+		sh_x=r.nextDouble()*(pic_x-200*ratio)+50*ratio;
+		sh_y=r.nextDouble()*(pic_y-200*ratio)+50*ratio;
 		spin=r.nextInt(2);
 
-		//System.out.println(spin);//only for testing
 		DShape fS=new DShape();
 		fS.drawShape(sh_x, sh_y, spin,ratio);
 		shape=fS.getRightShape();
 		shapeSwitch=fS.getFalseShape();
 
-		//画出假的阴影区形状,结果为falseShape
 		falseShape = fS.falseShape(sh_x, sh_y, pic_x, pic_y,ratio);
 
-		//将剪裁图形，shape形状之外的部分设置为透明
 		BufferedImage bi1 = ImageIO.read(new File(baseFilePathIn));
 		BufferedImage buffImg = new BufferedImage(bi1.getWidth(), bi1.getHeight(),
                 BufferedImage.TYPE_INT_ARGB);
@@ -96,7 +88,6 @@ public class ShadowShape {
 		g3.dispose();
 		ImageIO.write(buffImg, picPieceType, pieceFile);
 
-		//剪切旋转后的图片碎片
 		BufferedImage buffImgf = new BufferedImage(bi1.getWidth(), bi1.getHeight(),
                 BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g4 = buffImgf.createGraphics();
@@ -108,91 +99,81 @@ public class ShadowShape {
 		g4.dispose();
 		ImageIO.write(buffImgf, picPieceType, pieceFileSwitch);
 
-		//读取图片，并进行大致的剪裁，减为一个小矩形
 		FileInputStream fis = null ;
-        	ImageInputStream iis =null ;
+    ImageInputStream iis =null ;
 
-		fis = new FileInputStream(getPieceFile); //读取图片文件
+		fis = new FileInputStream(getPieceFile);
 		Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName(picPieceType);
 		ImageReader reader = (ImageReader) it.next();
 
-		iis = ImageIO.createImageInputStream(fis);//获取图片流
+		iis = ImageIO.createImageInputStream(fis);
 		reader.setInput(iis,true) ;
 		ImageReadParam param = reader.getDefaultReadParam();
 
 		int R_x=(int)(sh_x-15*ratio);
 		int R_y=(int)(sh_y-15*ratio);
 
-		Rectangle rect = new Rectangle(R_x, R_y, (int)(120*ratio), (int)(120*ratio));//定义一个矩形
-		param.setSourceRegion(rect);  //进行剪裁
-		BufferedImage bi = reader.read(0,param);//提供一个 BufferedImage，将其用作解码像素数据的目标。
+		Rectangle rect = new Rectangle(R_x, R_y, (int)(120*ratio), (int)(120*ratio));
+		param.setSourceRegion(rect);
+		BufferedImage bi = reader.read(0,param);
 
-		//输出剪裁后的图片碎片
 		ImageIO.write(bi, picPieceType, pieceFile);
 
-		//剪裁旋转后的图片为一个矩形
 		fis = null ;
-        	iis =null ;
+    iis =null ;
 
-		fis = new FileInputStream(getPieceFileSwitch); //读取图片文件
+		fis = new FileInputStream(getPieceFileSwitch);
 		Iterator<ImageReader> itf = ImageIO.getImageReadersByFormatName(picPieceType);
 		ImageReader readerf = (ImageReader) itf.next();
 
-		iis = ImageIO.createImageInputStream(fis);//获取图片流
+		iis = ImageIO.createImageInputStream(fis);
 		readerf.setInput(iis,true) ;
 		ImageReadParam paramf = readerf.getDefaultReadParam();
 
-		paramf.setSourceRegion(rect);  //进行剪裁
-		BufferedImage bif = readerf.read(0,paramf);//提供一个 BufferedImage，将其用作解码像素数据的目标。
+		paramf.setSourceRegion(rect);
+		BufferedImage bif = readerf.read(0,paramf);
 		ImageIO.write(bif, picPieceType, pieceFileSwitch);
 
-		//读取图片，并打上阴影区，图片读入 见前
 		Graphics2D g2 = SImg.createGraphics();
 		rule=AlphaComposite.SRC_OVER;
-		alpha= 0.7f;  //阴影区透明度设置
+		alpha= 0.7f;
 		AlphaComposite composite=AlphaComposite.getInstance(rule, alpha);
 		g2.setComposite(composite);
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);   //使用 setRenderingHint 设置抗锯齿
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
 		ImageIO.write(bi, picPieceType, pieceFile);
 
-		//设置阴影颜色
 		int res=0;
-		res = bi.getRGB((int)(70*ratio), (int)(70*ratio)) & 0xFFFFFF;  //change ARGB into RGB,& 0xFFFFFF
-		//System.out.println(res);
-		int[] rgb = new int [3]; //set RGB, value of color
-        	rgb[0] = (res & 0xff0000) >> 16;
-        	rgb[1] = (res & 0xff00) >> 8;
-        	rgb[2] = (res & 0xff);
+		res = bi.getRGB((int)(70*ratio), (int)(70*ratio)) & 0xFFFFFF;
 
-        	if(rgb[0]<0) {
-        	rgb[0]=0;
-        	}
-        	else if(rgb[0]>255) {
-        	rgb[0]=255;
-        	}
-        	if(rgb[1]<0) {
-        	rgb[1]=0;
-        	}
-        	else if(rgb[1]>255) {
-        	rgb[1]=255;
-        	}
-        	if(rgb[2]<0) {
-        	rgb[0]=0;
-        	}
-        	else if(rgb[0]>255) {
-        	rgb[0]=255;
-        	}
-        	//only for testing
-        	//System.out.println(rgb[0]);
-        	//System.out.println(rgb[1]);
-        	//System.out.println(rgb[2]);
+		int[] rgb = new int [3];
+    rgb[0] = (res & 0xff0000) >> 16;
+    rgb[1] = (res & 0xff00) >> 8;
+    rgb[2] = (res & 0xff);
 
-        	Color color=new Color(rgb[0],rgb[1],rgb[2]);
-		g2.setPaint(color);//阴影区颜色设置
+    if(rgb[0]<0) {
+      rgb[0]=0;
+    }
+    else if(rgb[0]>255) {
+      rgb[0]=255;
+    }
+    if(rgb[1]<0) {
+    	rgb[1]=0;
+    }
+    else if(rgb[1]>255) {
+    	rgb[1]=255;
+    }
+    if(rgb[2]<0) {
+    	rgb[0]=0;
+    }
+    else if(rgb[0]>255) {
+    	rgb[0]=255;
+    }
+
+    Color color=new Color(rgb[0],rgb[1],rgb[2]);
+		g2.setPaint(color);
 		g2.fill(shape);
 
-		//打上假的阴影区
 		g2.fill(falseShape);
 		g2.dispose();
 		ImageIO.write(SImg, picType, shadowFile);
@@ -201,8 +182,8 @@ public class ShadowShape {
 	class DShape{
 		private double f_x;
 		private double f_y;
-		private Shape shapef;  //正确剪裁与阴影区形状
-		private Shape shaper;  //旋转后剪裁形状
+		private Shape shapef;
+		private Shape shaper;
 		public void drawShape(double sh_x,double sh_y,int spin,double ratio) {
 			sh_x-=15*ratio;
 			sh_y-=15*ratio;
@@ -210,10 +191,10 @@ public class ShadowShape {
 			Shape shape1 = new RoundRectangle2D.Double(15*ratio+sh_x,15*ratio+sh_y,80*ratio,80*ratio,20*ratio,20*ratio);
 			Area a=new Area(shape1);
 			Area b=new Area(shape1);
-			Area shape2 = new Area(new Ellipse2D.Double(25*ratio+sh_x, 0+sh_y, 30*ratio, 30*ratio));//上
-			Area shape3 = new Area(new Ellipse2D.Double(80*ratio+sh_x, 25*ratio+sh_y, 30*ratio, 30*ratio));//右
-			Area shape4 = new Area(new Ellipse2D.Double(0+sh_x, 55*ratio+sh_y, 30*ratio, 30*ratio));//左
-			Area shape5 = new Area(new Ellipse2D.Double(55*ratio+sh_x, 80*ratio+sh_y, 30*ratio, 30*ratio));//下
+			Area shape2 = new Area(new Ellipse2D.Double(25*ratio+sh_x, 0+sh_y, 30*ratio, 30*ratio));
+			Area shape3 = new Area(new Ellipse2D.Double(80*ratio+sh_x, 25*ratio+sh_y, 30*ratio, 30*ratio));
+			Area shape4 = new Area(new Ellipse2D.Double(0+sh_x, 55*ratio+sh_y, 30*ratio, 30*ratio));
+			Area shape5 = new Area(new Ellipse2D.Double(55*ratio+sh_x, 80*ratio+sh_y, 30*ratio, 30*ratio));
 
 			switch(spin) {
 			case 0:
@@ -253,13 +234,11 @@ public class ShadowShape {
 		}
 
 		public Shape falseShape(double sh_x,double sh_y,double pic_x,double pic_y,double ratio) {
-			/*
-			画假的阴影区形状
-			*/
-			Random r=new Random();//设立假值
+
+			Random r=new Random();
 			f_x=r.nextDouble()*(pic_x-200*ratio)+50*ratio;
 			f_y=r.nextDouble()*(pic_y-200*ratio)+50*ratio;
-			int spinf=r.nextInt(2);//假值的旋转参数
+			int spinf=r.nextInt(2);
 
 			while((f_x<(sh_x+150*ratio))&&(f_x>(sh_y-150*ratio))&&(f_y<(sh_y+150*ratio))&&(f_y>(sh_y-150*ratio))) {
 				f_x=r.nextDouble()*(pic_x-200*ratio)+50*ratio;
@@ -269,10 +248,10 @@ public class ShadowShape {
 			f_y-=15*ratio;
 			Shape shapef1 = new RoundRectangle2D.Double(15*ratio+f_x,15*ratio+f_y,80*ratio,80*ratio,20*ratio,20*ratio);
 			Area b=new Area(shapef1);
-			Area shapef2 = new Area(new Ellipse2D.Double(25*ratio+f_x, 0+f_y, 30*ratio, 30*ratio));//上
-			Area shapef3 = new Area(new Ellipse2D.Double(80*ratio+f_x, 25*ratio+f_y, 30*ratio, 30*ratio));//右
-			Area shapef4 = new Area(new Ellipse2D.Double(0+f_x, 55*ratio+f_y, 30*ratio, 30*ratio));//左
-			Area shapef5 = new Area(new Ellipse2D.Double(55*ratio+f_x, 80*ratio+f_y, 30*ratio, 30*ratio));//下
+			Area shapef2 = new Area(new Ellipse2D.Double(25*ratio+f_x, 0+f_y, 30*ratio, 30*ratio));
+			Area shapef3 = new Area(new Ellipse2D.Double(80*ratio+f_x, 25*ratio+f_y, 30*ratio, 30*ratio));
+			Area shapef4 = new Area(new Ellipse2D.Double(0+f_x, 55*ratio+f_y, 30*ratio, 30*ratio));
+			Area shapef5 = new Area(new Ellipse2D.Double(55*ratio+f_x, 80*ratio+f_y, 30*ratio, 30*ratio));
 			spinf=(spinf==1)?0:1;
 			switch(spinf) {
 			case 0:
